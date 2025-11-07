ce01821449d268150cc04ef93539f292c772b094
andreuvidalf <andreuvidalf@alumnat.iesdamiahuget.net>
2025-11-07 11:34:53 +0100

    normal cistella sense desar

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Happy developing ✨')
+let totaly = 0.0000000
+
+function Comprar (nom, preu){
+    document.getElementById('cistella').innerText += nom + " - " + preu +"€\n"
+    totaly = totaly + preu
+    totaly.toFixed(2);
+    document.getElementById('PreuTotal').innerText = totaly.toFixed(2);
+}
+
+
+
+
